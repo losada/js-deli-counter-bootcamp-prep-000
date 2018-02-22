42cfbc6ba19ed95a045ca08e919dcb178c734308
Michael Losada <michael.losada@gmail.com>
2018-02-22 20:24:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,16 +19,6 @@ function nowServing(katzDeli){
 
 var line = [];
 
-/*function currentLine(){
-  for(let i = 0; i < katzDeli.length; i++){
-    line.push(`${i+1}. ${katzDeli[i]}`);
-  }
-  if (katzDeli.length === 0) {
-    return "The line is currently empty.";
- } else
- return (`The line is currently: ${line}`);
-}*/
-
 function currentLine(katzDeli) {
   let i = 0;
   while (i < katzDeli.length) {
